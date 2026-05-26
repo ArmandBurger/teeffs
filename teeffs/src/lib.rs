33c9f3ada00b2bc5f776b8b127d0d5068d57ff0e
ArmandBurger <22417562+ArmandBurger@users.noreply.github.com>
2026-05-26 08:57:14 +0200

    setup cargo workspace for port.

---
+++ teeffs/src/lib.rs
@@ -0,0 +1,18 @@
+pub use teeffs_core    as core;
+pub use teeffs_grid    as grid;
+pub use teeffs_mesh    as mesh;
+pub use teeffs_algo    as algo;
+pub use teeffs_deform  as deform;
+pub use teeffs_voronoi as voronoi;
+pub use teeffs_export  as export;
+
+#[allow(unused_imports)]
+pub mod prelude {
+    pub use crate::core::*;
+    pub use crate::grid::*;
+    pub use crate::mesh::*;
+    pub use crate::algo::*;
+    pub use crate::deform::*;
+    pub use crate::voronoi::*;
+    pub use crate::export::*;
+}
